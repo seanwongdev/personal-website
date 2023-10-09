565b1407527241b3429dfb6addc2be8282b0f933
seanwongeth <sean.wong.2009@business.smu.edu.sg>
2023-10-09 20:38:04 +0800

    updated

--- src/js/index.js
+++ src/js/index.js
@@ -4,3 +4,15 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+const typed = new Typed("#element", {
+  strings: [
+    "Learning is a life long process",
+    "Learning coding is a long process",
+    "...Coding is <strong>Life</strong>",
+  ],
+  startDelay: 1000,
+  backDelay: 1500,
+  typeSpeed: 20,
+  loop: true,
+});
